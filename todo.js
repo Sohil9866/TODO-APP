ca5a6dde25f691645fd206b6c86d0966140b1ce4
Sohil9866 <sohilshresthac@gmail.com>
2025-06-15 10:43:08 +0545

    feat add js for form log

---
+++ todo.js
@@ -0,0 +1,14 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevents page reload
+
+  const form = event.target;
+  const title = form.elements["title"].value;
+  const description = form.elements["description"].value;
+  const priority = form.elements["priority"].value;
+  const deadline = form.elements["deadline"].value;
+
+  console.log("title:", title);
+  console.log("description:", description);
+  console.log("priority:", priority);
+  console.log("deadline:", deadline);
+});
